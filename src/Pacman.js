871cfc64c71e4ce040d6ef6d1a67dc10c74bba12
LukasKubica <36824415+LukasKubica@users.noreply.github.com>
2023-04-03 18:36:04 +0200

    Update Pacman.js

--- src/Pacman.js
+++ src/Pacman.js
@@ -74,16 +74,16 @@ export default class Pacman {
 
   #loadPacmanImages() {
     const pacmanImage1 = new Image();
-    pacmanImage1.src = "images/pac0.png";
+    pacmanImage1.src = "Images/pac0.png";
 
     const pacmanImage2 = new Image();
-    pacmanImage2.src = "images/pac1.png";
+    pacmanImage2.src = "Images/pac1.png";
 
     const pacmanImage3 = new Image();
-    pacmanImage3.src = "images/pac2.png";
+    pacmanImage3.src = "Images/pac2.png";
 
     const pacmanImage4 = new Image();
-    pacmanImage4.src = "images/pac0.png";
+    pacmanImage4.src = "Images/pac0.png";
 
     this.pacmanImages = [
       pacmanImage1,
